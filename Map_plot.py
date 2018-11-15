3d39606280cd94bf9b4b206976caeabd44369140
Michiya Inagaki <inagaki@sens.sys.es.osaka-u.ac.jp>
2018-11-15 17:59:43 +0900

    プロットを綺麗にしました

--- Map_plot.py
+++ Map_plot.py
@@ -12,7 +12,7 @@ from mpl_toolkits.mplot3d.axes3d import Axes3D
 csv_L=[list(map(float,line.rstrip(",\n").split(","))) for line in open('C:/Users/SENS/source/repos/Control_PTU/Control_PTU/csv/Ex1/Split_OP.csv').readlines()]
 csv_y=[list(map(float,line.rstrip().split(","))) for line in open('C:/Users/SENS/source/repos/Control_PTU/Control_PTU/csv/Ex1/Split_OP_y.csv').readlines()]
 #xの事前分布の強度/////////////////
-ramuda=0.1
+ramuda = 1.0
 #ロボットプラットフォームの寸法
 Height = 0.273 + 0.01 + 0.091
 length_tilt = 0.038 + 0.01 + 0.02
@@ -82,27 +82,70 @@ temp_concentratin_list = list(sol["x"])
 x_list=[]
 y_list=[]
 z_list=[]
+x_fine_list=[]
+y_fine_list=[]
+z_fine_list=[]
 concentration_list = []
 
 #マッピング用リストの作成 #計測範囲内のみマップを作成する
 for num in range(cell_size):
-    #ボクセル座標出す => xyz座標に直す => 位置補正する
+    '''
+    #ボクセル座標出す => xyz座標に直す => 位置補正する => ボクセルの中心に点を置く
     x_point = int(((num % (Xrange*Yrange)) % Xrange)) * delta + xmin + (delta/2)
-    y_point = int(((num % (Xrange*Yrange)) / Xrange)) * delta + (delta/2)
+    y_point = int(((num % (Xrange*Yrange)) / Xrange)) * delta + (delta/2) -ymin
     z_point = int((num / (Xrange*Yrange))) * delta + (delta/2)
     print('num',num, 'x',x_point,'y',y_point,'z',z_point)
-    if int(num % (Xrange*Yrange) / Xrange) >= RangetoNum(ymin):
-        print("huga")
+    if int(num % (Xrange*Yrange) / Xrange) >= RangetoNum(ymin): #and temp_concentratin_list[num] >= 200 :
         x_list.append(x_point)
         y_list.append(y_point)
         z_list.append(z_point)
         concentration_list.append(temp_concentratin_list[num])
-print(ymin)
-print(RangetoNum(ymin))
+    '''
+
+    #点群でボクセルを表現する
+    x_point = int(((num % (Xrange*Yrange)) % Xrange)) * delta + xmin
+    y_point = int(((num % (Xrange*Yrange)) / Xrange)) * delta -ymin
+    z_point = int((num / (Xrange*Yrange))) * delta
+    print('num',num, 'x',x_point,'y',y_point,'z',z_point)
+    range_num = 5
+    if int(num % (Xrange*Yrange) / Xrange) >= RangetoNum(ymin) and temp_concentratin_list[num] >= 400 : 
+        for numz in range(range_num):
+            for numy in range(range_num):
+                for numx in range(range_num):
+                    x_fine_list.append(x_point+numx*float(delta/range_num))
+                    y_fine_list.append(y_point+numy*float(delta/range_num))
+                    z_fine_list.append(z_point+numz*float(delta/range_num))
+                    concentration_list.append(temp_concentratin_list[num])
+
+
 # 散布図を表示、各点の色を濃度に対応させる
 fig = plt.figure()
 ax = Axes3D(fig)
-p = ax.scatter(x_list, y_list, z_list, s=10, c=concentration_list, cmap='Blues')
+
+# 表示範囲の設定
+ax.set_xlim(-1.0, 1.0)
+ax.set_ylim(0, 2)
+ax.set_zlim(0, 0.6)
+
+#刻み幅の設定
+ax.set_xticks(np.arange(-1.0, 1.0, 0.2))
+ax.set_yticks(np.arange(0.0, 2.0, 0.2))
+ax.set_zticks(np.arange(0.0, 0.6, 0.2))
+
+#背景色：引数RGBA
+ax.w_xaxis.set_pane_color((0., 0., 0., 0.5))
+ax.w_yaxis.set_pane_color((0., 0., 0., 0.5))
+ax.w_zaxis.set_pane_color((0., 0., 0., 0.5))
+
+#縦横比 np.diag([x軸, y軸, z軸, 1])
+aff = np.diag([1.0, 1.0, 0.3, 1])
+aff[0][3] = 0#x軸担当
+aff[1][3] = 0#y軸担当
+aff[2][3] = 0#z軸担当
+ax.get_proj = lambda: np.dot(Axes3D.get_proj(ax), aff)
+
+#p = ax.scatter(x_list, y_list, z_list, s=10, c=concentration_list, cmap='hot')
+p = ax.scatter(x_fine_list, y_fine_list, z_fine_list, s=1, c=concentration_list, cmap='Blues')
 # ラベルの設定
 #plt.title("Graph Title")
 ax.set_xlabel("X-axis")
@@ -110,5 +153,6 @@ ax.set_ylabel("Y-axis")
 ax.set_zlabel("Z-axis")
 # カラーバーを表示
 plt.colorbar(p)
+
 # プロット
 plt.show()
